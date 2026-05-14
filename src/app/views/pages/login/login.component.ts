58a9b683172dc6e19aecef9b7b28d66294a35e18
michell_antonionny <michell@f2lti.com.br>
2026-05-14 14:28:54 -0300

    first commit

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IconDirective } from '@coreui/icons-angular';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CardModule, ButtonModule, GridModule, FormModule } from '@coreui/angular';
 import {
   ButtonDirective,
   CardBodyComponent,
@@ -13,10 +15,37 @@ import {
   InputGroupTextDirective,
   RowComponent
 } from '@coreui/angular';
+import {IconDirective} from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective]
+  standalone: true,
+  imports: [
+    FormsModule,
+    CardModule,
+    ButtonModule,
+    GridModule,
+    FormModule,ContainerComponent, RowComponent, ColComponent, CardGroupComponent, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective],
+  templateUrl: './login.component.html'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.post<any>('http://localhost:8080/auth/login', {
+      username: this.username,
+      password: this.password
+    }).subscribe({
+      next: (res: any) => {
+        console.log('Login OK', res);
+        // redirecionar para dashboard
+      },
+      error: (err: any) => {
+        console.error('Erro no login', err);
+      }
+    });
+  }
+}
